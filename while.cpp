56f153d5b303b4481a13e1844d9410140dea8dc8
swastik2703 <swastikvasistha2127@gmail.com>
2021-06-05 19:40:57 -0400

    while loop

---
+++ while.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=0;
+    cout<<"the while loop is working\n";
+    while(i<5)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+}
