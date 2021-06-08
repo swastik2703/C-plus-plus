a769597418a94e02c5633fbb616f0e2e8ae25bc4
swastik2703 <swastikvasistha2127@gmail.com>
2021-06-07 20:51:55 -0400

    if-else working

---
+++ ifelse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cout<<"checking no is even or odd"<<endl;
+    cin>>i;
+    if(i%2==0)
+    {
+        cout<<"even";
+    }
+    else
+    {
+        cout<<"odd";
+    }
+}
