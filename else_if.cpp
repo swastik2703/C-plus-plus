eac7f14664c9feff1422f6031ebaafad54955f5b
swastik2703 <swastikvasistha2127@gmail.com>
2021-06-13 21:06:38 -0400

    else if

---
+++ else_if.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    if(x==y)
+    {
+        cout<<"scores are equal";
+    }
+    else if(x>y)
+    {
+        cout<<"x scores more";
+    }
+    else{
+        cout<<"y scores more";
+    }
+    
+}
