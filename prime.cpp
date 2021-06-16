acb4a300833af338b8687039b85a9afd4eb1a796
swastik2703 <swastikvasistha2127@gmail.com>
2021-06-16 13:47:05 -0400

    prime no or not

---
+++ prime.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+// int main()
+// {
+//     int n,i=2;
+//     cout<<"enter the no:";
+//     cin>>n;
+//     if(n%i==0)
+//     {
+//         cout<<"not prime";
+//     }
+//     else
+//     {
+//         cout<<"prime";
+//     }
+// }
+
+int main() {
+ int n;
+ cout<<"Enter a number: ";
+ cin>>n;
+ 
+ int i;
+ for (i=2; i<n; i++) {
+ if (n%i==0) {
+
+ cout<<"Not a prime"<<endl;
+ break;
+ }
+ }
+ if (i==n) { 
+     
+ cout<<"Prime Number"<<endl;
+ }
+ 
+ return 0;
+}
