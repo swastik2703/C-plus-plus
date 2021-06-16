8e9700fb52037ce71c87622fe7e1dc44e420bf38
swastik2703 <swastikvasistha2127@gmail.com>
2021-06-15 23:38:17 -0400

    multiply table

--- multiply.cpp
+++ multiply.cpp
@@ -1,14 +1,29 @@
 #include<iostream>
 using namespace std;
 
+// int main()
+// {
+//     int n,i;
+//     int mult;
+//     cin>>n;
+
+//     for(i=1;i<=10;i++)
+//     {
+//         cout<<n<<"*"<<i<<"="<<n*i<<endl;
+//     }
+// }
+
+
 int main()
 {
-    int n,i;
-    int mult;
+    int n;
+    int i=1;
     cin>>n;
 
-    for(i=1;i<=10;i++)
+    while(i<=10)
     {
         cout<<n<<"*"<<i<<"="<<n*i<<endl;
+        i++;
     }
+
 }
