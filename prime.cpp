789820596a34ed40850db69aa4752df0c3b835c9
swastik2703 <swastikvasistha2127@gmail.com>
2021-06-16 21:48:19 -0400

    prime no

--- prime.cpp
+++ prime.cpp
@@ -1,21 +1,6 @@
 #include<iostream>
 using namespace std;
 
-// int main()
-// {
-//     int n,i=2;
-//     cout<<"enter the no:";
-//     cin>>n;
-//     if(n%i==0)
-//     {
-//         cout<<"not prime";
-//     }
-//     else
-//     {
-//         cout<<"prime";
-//     }
-// }
-
 int main() {
  int n;
  cout<<"Enter a number: ";
