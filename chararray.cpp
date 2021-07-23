086a10d88ab540a102445e0848ec105c500d6689
swastik2703 <swastikvasistha2127@gmail.com>
2021-07-23 19:46:17 -0400

    print character array

---
+++ chararray.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+// int main()
+// {
+//     char a[50]="SWASTIK";
+//     int i=0;
+
+//     while(a[i]!='\0')
+//     {
+//         cout<<a[i]<<" ";
+//         i++;
+//     }
+// }
+
+
+
+int main()
+{
+    char a[50];
+    cin>>a;
+    cout<<a;
+}
