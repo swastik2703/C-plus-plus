1cd89a7c3a3209ae668567029341ef1c8952ef67
swastik2703 <swastikvasistha2127@gmail.com>
2021-08-05 11:31:28 -0400

    string append

---
+++ stringappend.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    // string str1="thank";
+    // string str2="full";
+
+    // cout<<str1.append(str2);
+
+
+    // string s1="command";
+    // string s2="window";
+    // cout<<s1+s2;
+
+    // string s1="fam";
+    // string s2="ily";
+    // s1.clear();         //clear function helps to delete string
+    // cout<<s1;
+
+    string str="abc";
+    string str1="xyz";
+    cout<<str.compare(str1);
+   
+
+}
