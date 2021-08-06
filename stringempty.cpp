636eb03df7d3bb6d1dac29d9d8970a2c44160725
swastik2703 <swastikvasistha2127@gmail.com>
2021-08-06 18:58:53 -0400

    string empty function

---
+++ stringempty.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    // string s1="saxndxdb";
+    // s1.clear();
+    // if(s1.empty())
+    // {
+    //     cout<<"string is empty";
+    // }
+
+    string s1="saxndxdb";
+    if(!s1.empty())
+    {
+        cout<<"string is not empty";
+    }
+}
