003d7350f155fe3393c9124cbed1c6d8a3234c34
swastik2703 <swastikvasistha2127@gmail.com>
2021-08-06 19:18:49 -0400

    string size and length function

---
+++ stringsizelen.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    // string s1="swastikvasu";
+    // cout<<s1.size();
+    
+    string s="swastikhero";
+    for(int i=0;i<s.length();i++)
+    {
+        cout<<s[i]<<endl;
+    }
+}
