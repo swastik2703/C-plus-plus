fbe6eb6e23ab741f89d20705913634863e89b20c
swastik2703 <swastikvasistha2127@gmail.com>
2021-08-07 20:03:26 -0400

    covert string into uppercase or lowercase

---
+++ stringuplowconver.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+// int main()
+// {
+//     string s="swastikvasistha";
+//     //upercase
+//     for(int i=0;i<s.size();i++)
+//     {
+//         if(s[i]>='a' && s[i]<='z')
+//         {
+//             s[i]-=32;       //since 'a'-'A'=32     i.e a is 32 times greater than A
+//         }
+//     }
+//     cout<<s<<endl;
+
+
+//     //lowercase
+//     for(int i=0;i<s.size();i++)
+//     {
+//         if(s[i]>='A' && s[i]<='Z')
+//         {
+//             s[i]+=32;
+//         }
+//     }
+
+//     cout<<s;
+// }
+
+
+
+
+int main()
+{
+    string s="fgssbjbffxccbga";
+    transform(s.begin(),s.end(),s.begin(),::toupper);
+    cout<<s<<endl;
+
+    transform(s.begin(),s.end(),s.begin(),::tolower);
+    cout<<s;
+}
