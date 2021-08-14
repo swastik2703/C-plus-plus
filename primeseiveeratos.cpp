f6781a722eeb17638f092b4d72b989e84e1755ae
swastik2703 <swastikvasistha2127@gmail.com>
2021-08-14 13:59:10 -0400

    seive eratosthenes find prime no

--- primeseiveeratos.cpp
+++ primeseiveeratos.cpp
@@ -4,22 +4,22 @@ using namespace std;
 void primeseive(int n)
 {
     int prime[100]={0};     //100 no array initialise with 0 index
-    for(int i=2;i<=n;i++)
+    for(int i=2;i<=n;i++)   //starting with 2 bcuz its a prime no
     {
-        if(prime[i]==0)
+        if(prime[i]==0)     //condition for unmarked
         {
-            for(int j=i*i;j<=n;j+=i)
+            for(int j=i*i;j<=n;j+=i)    //we need multiples thatswhy i^2
             {
-                prime[j]=1;
+                prime[j]=1;       //marked no's
             }
         }
     }
 
     for(int i=2;i<=n;i++)
     {
-        if(prime[i]==0)
+        if(prime[i]==0)         //if unmarked 
         {
-            cout<<i<<" ";
+            cout<<i<<" ";       //print those no's
         }
         
     }
