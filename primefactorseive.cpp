33d7c595d506f89f38f04ab58036414a8ce9d025
swastik2703 <swastikvasistha2127@gmail.com>
2021-08-15 14:15:21 -0400

    seive eratosthenes find prime factor

---
+++ primefactorseive.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void primefactor(int n)
+{
+    int a[100]={0};     //initialise with 0
+    for(int i=2;i<=n;i++)
+    {
+        a[i]=i;     //hr no khud ka prime factor hoga
+    }
+
+    for(int i=2;i<=n;i++)
+    {
+        if(a[i]==i)     //check agar unmarked hai 
+        {
+            for(int j=i^2;j<=n;j+=i)
+            {
+                if(a[j]==j)     //j mai enter krne ke baad j ko check kro vo unmarked hai if yes
+                {
+                    a[j]=i;     //swap
+                }
+            }
+        }
+    }
+
+    while(n!=1)
+    {
+        cout<<a[n]<<" ";
+        n=n/a[n];
+    }
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    primefactor(n);
+}
