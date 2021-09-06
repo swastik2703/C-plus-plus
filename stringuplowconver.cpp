0e2c2a808f6b02e3cd0de1dd7d91ae0dd794b1ea
swastik2703 <swastikvasistha2127@gmail.com>
2021-09-06 17:50:50 +0530

    recursive fibonacii

--- stringuplowconver.cpp
+++ stringuplowconver.cpp
@@ -35,10 +35,12 @@ using namespace std;
 
 int main()
 {
-    string s="fgssbjbffxccbga";
+    string s="dfgsfhiwegisndhs";
     transform(s.begin(),s.end(),s.begin(),::toupper);
     cout<<s<<endl;
+    
 
     transform(s.begin(),s.end(),s.begin(),::tolower);
     cout<<s;
+    
 }
