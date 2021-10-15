78d383b5aa353156e998ada8e6e81425d1f484a7
swastik2703 <swastikvasistha2127@gmail.com>
2021-10-15 10:28:25 +0530

    quick sort

---
+++ quicksort.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+void swap(int arr[],int i,int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int partition(int arr[],int l ,int r)
+{
+    int pivot = arr[r];
+    int i = l-1;
+
+    for(int j=l;j<r;j++)
+    {
+        if(arr[j]<pivot){
+            i++;
+            swap(arr,i,j);
+        }
+    }
+
+    swap(arr,i+1,r);
+    return i+1;
+}
+
+void quicksort(int arr[],int l ,int r)
+{
+    if(l<r)
+    {
+        int pi = partition(arr,l,r);
+        quicksort(arr,l,pi-1);
+        quicksort(arr,pi+1,r);
+    }
+}
+
+int main()
+{
+    int arr[] = {6,3,9,5,2,8,7,11,1};
+    int n = 9;
+    quicksort(arr,0,8);
+    for(int i=0;i<9;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
