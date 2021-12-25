fed600943fc3543e6ebee34b5fee02525faa9cd5
swastik2703 <swastikvasistha2127@gmail.com>
2021-12-25 22:07:05 +0530

    binary to decimal

--- binarytodecimal_fn.cpp
+++ binarytodecimal_fn.cpp
@@ -1,24 +1,49 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
-void binarytodecimal(int n)
-{
-    int sum=0;
-    int x=1;
-    while(n>0)
-    {
-        int y=n%10;
-        sum+=x*y;
-        x*=2;
-        n/=10;
-    }
-    cout<<sum;
-    return;
-}
+// void binarytodecimal(int n)
+// {
+//     int sum=0;
+//     int x=1;
+//     while(n>0)
+//     {
+//         int y=n%10;
+//         sum+=x*y;
+//         x*=2;
+//         n/=10;
+//     }
+//     cout<<sum;
+//     return;
+// }
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     binarytodecimal(n);
+// }
+
 
 int main()
 {
-    int n;
+    int n,i=0;
     cin>>n;
-    binarytodecimal(n);
+    int ans = 0;
+
+
+    while(n!=0)
+    {
+        int digit = n%10;
+
+        if(digit == 1)
+        {
+            ans = ans + pow(2,i);
+        }
+
+        n = n/10;
+        i++;
+    }
+
+    cout<<ans;
 }
