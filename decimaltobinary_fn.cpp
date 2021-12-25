05cb3cf362ea4601e036b185f4123dd4b99e266d
swastik2703 <swastikvasistha2127@gmail.com>
2021-12-25 21:55:05 +0530

    logical operators uses

--- decimaltobinary_fn.cpp
+++ decimaltobinary_fn.cpp
@@ -1,15 +1,33 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 int main() {
-    int n,binary=0,i=1,rem;
+    // int n,binary=0,i=1,rem;
+	// cin>>n;
+	// while(n!=0)
+	// {
+	// 	rem=n%2;
+	// 	binary=binary+(rem*i);
+    //     n=n/2;
+	// 	i*=10;
+	// }
+	// cout<<binary;
+
+
+	int n;
 	cin>>n;
+	int ans = 0;
+	int i=0;
+
+
 	while(n!=0)
 	{
-		rem=n%2;
-		binary=binary+(rem*i);
-        n=n/2;
-		i*=10;
+		int bit = n&1;
+		ans = (bit * pow(10,i) + ans);
+		n = n>>1;
+		i++;
 	}
-	cout<<binary;
+
+	cout<<"answer is :"<<ans;
 }
