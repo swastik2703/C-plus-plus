ca64be219fb40ec8d161b24b6837295d4b2a2e55
swastik2703 <swastikvasistha2127@gmail.com>
2022-04-14 11:50:28 +0530

    length of a string

---
+++ char_str/string_len.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int getlength(char name[]){
+    int count = 0;
+    for(int  i = 0; name[i] != '\0'; i++){
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    char name[20];
+    cout<<"enter your name: "<<endl;
+    cin>>name;
+    cout<<"length:" << getlength(name)<<endl;
+}
