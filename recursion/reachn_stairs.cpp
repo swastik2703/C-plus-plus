0f4939fb86e46bfe8dbbaeb5887ebe68f6458aa0
swastik2703 <swastikvasistha2127@gmail.com>
2022-05-03 01:01:13 +0530

    reach to n stairs by one step or two recursion

---
+++ recursion/reachn_stairs.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int countDistinctWayToClimbStair(long long nStairs)
+{
+    if(nStairs < 0){
+		return 0;
+	}
+	
+	if(nStairs == 0){
+		return 1;
+	}
+	
+	int ans = countDistinctWayToClimbStair(nStairs-1) + countDistinctWayToClimbStair(nStairs-2);
+		return ans;
+}
+
+int main(){
+
+    int nStairs;
+    cin>>nStairs;
+    countDistinctWayToClimbStair(nStairs);
+}
+
+
