d32d20c0475649fbcc38d4bca96cd558f6add403
swastik2703 <swastikvasistha2127@gmail.com>
2022-07-05 13:13:01 +0530

    Diagonal Traversal in a tree TC O(n) SC O(height of tree)

---
+++ Trees/DiagonalTraversal.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; 
+
+
+vector<int> diagonal(Node *root)
+{
+   queue<Node*> q;
+   vector<int> ans;
+   
+   if(root == NULL){
+       return ans;
+   }
+   
+   q.push(root);
+   
+   while(!q.empty()){
+       Node* temp = q.front();
+       q.pop();
+       
+       while(temp){
+           if(temp -> left){
+               q.push(temp -> left);
+           }
+           ans.push_back(temp -> data);
+           temp = temp -> right;
+       }
+   }
+   return ans;
+    
+}
