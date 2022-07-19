293f247838ac5cdf3685f4337bcb536978e16ea5
swastik2703 <swastikvasistha2127@gmail.com>
2022-07-19 14:29:19 +0530

    insertion deletion heapify in a heap

--- Heap/intro.cpp
+++ Heap/intro.cpp
@@ -166,19 +166,54 @@ class heap{
 
 };
 
+//heapify function
+void heapify(int arr[], int n, int i){
+
+    int largest = i;    // ith element ko sahi jagah pe leke jana hai
+    int left = 2*i;
+    int right = 2*i + 1;
+
+    if(left < n && arr[largest] < arr[left]){
+        largest = left;
+    }
+
+    if(right < n && arr[largest] < arr[right]){
+        largest = right;
+    }
+
+    //largest update hogya ya nhi
+    if(largest != i){
+        swap(arr[largest],arr[i]);
+        heapify(arr,n,largest);
+    }
+}
+
+
 int main(){
     heap h;
     h.insert(50);
-    h.insert(40);
-    h.insert(35);
-    h.insert(25);
-    h.insert(20);
-    h.insert(27);
-    h.insert(23);
+    h.insert(55);
+    h.insert(53);
+    h.insert(52);
+    h.insert(54);
     h.print();
 
     h.deleteFromHeap();
     h.print();
+
+    int arr[6] = {-1,54,53,55,52,50};
+    int n = 5;
+
+    for(int i=n/2; i>0; i--){
+        heapify(arr,n,i);
+    }
+        
+
+    cout<<"Heapify algo works properly"<<endl;
+    for(int i=1; i<=n; i++){
+        cout<<arr[i]<<" ";
+    }
+       cout<<endl;
 
     return 0;
 }
