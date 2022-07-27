773e22d59a870061cc240720e2401da8c9278258
swastik2703 <swastikvasistha2127@gmail.com>
2022-07-27 11:31:29 +0530

    Normal implementation of Longest common prefix using for loop

---
+++ Trie/longestCommonPref.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+string longestCommonPrefix(vector<string> &arr, int n)
+{
+    string ans = "";
+    
+    //for storing first string
+    for(int i=0; i<arr[0].length(); i++){
+        char ch = arr[0][i];
+        
+        bool match = true;
+        
+        for(int j=1; j<n; j++){
+            //if not match
+            if(arr[j].size() < i || ch != arr[j][i]){
+                match = false;
+                break;
+            }
+        }
+        
+        if(match == false)
+            break;
+        else
+            ans.push_back(ch);
+    }
+    return ans;
+}
+
+
