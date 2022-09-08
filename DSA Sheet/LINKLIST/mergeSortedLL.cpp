bb8237137580e40602c21937204643554639aac5
swastik2703 <swastikvasistha2127@gmail.com>
2022-09-09 00:00:12 +0530

    merge 2 sorted link list

---
+++ DSA Sheet/LINKLIST/mergeSortedLL.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL)
+            return list2;
+        if(list2 == NULL)
+            return list1;
+        ListNode* ans,*tail;
+        
+        if(list1 -> val<list2 -> val){
+            ans = list1;
+            tail = list1;
+            list1 = list1 -> next;
+        }
+        
+        else{
+            ans = list2;
+            tail = list2;
+            list2 = list2 -> next;
+        }
+        
+        while(list1 != NULL && list2 != NULL){
+            if(list1 -> val < list2 -> val){
+                tail -> next = list1;
+                tail = list1;
+                list1 = list1 -> next;
+            }
+            
+            else{
+                tail -> next = list2;
+                tail = list2;
+                list2 = list2->next;
+                
+            }
+            
+            
+        }
+        if(list1 == NULL)
+            tail -> next = list2;
+        else
+            tail -> next = list1;
+        return ans;
+    }
+};
