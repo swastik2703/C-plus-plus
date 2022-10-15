30baeb382b05b459e7ece9bebc7028db1c96ad46
swastik2703 <swastikvasistha2127@gmail.com>
2022-10-15 16:09:25 +0530

    Merging 2 link list

---
+++ Leetcode Challenge/merge2LL.cpp
@@ -0,0 +1,81 @@
+#include<list>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    //*********** APPROACH-1 ********
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        if(list1 == NULL)
+            return list2;
+        if(list2 == NULL)
+            return list1;
+        
+        if(list1 -> val < list2 -> val){
+            list1 -> next = mergeTwoLists(list1->next,list2);
+            return list1;
+        }
+            
+        else{
+            list2 -> next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+            
+    }
+
+    //********** APPROACH-2 ********
+        
+//         //base case check kro
+//         if(list1 == NULL)
+//             return list2;
+//         if(list2 == NULL)
+//             return list1;
+        
+//         ListNode* ans,*tail;
+        
+//         if(list1 -> val < list2 -> val){
+//             ans = list1;
+//             tail = list1;
+//             list1 = list1 -> next;
+//         }
+//         else{
+//             ans = list2;
+//             tail = list2;
+//             list2 = list2 -> next;
+            
+//         }
+        
+//         while(list1 != NULL && list2 != NULL){
+//             if(list1 -> val < list2 -> val){
+//                 tail -> next = list1;
+//                 tail = list1;
+//                 list1 = list1 -> next;
+//             }
+//             else{
+//                 tail->next = list2;
+//                 tail = list2;
+//                 list2 = list2 -> next;
+                
+//             }
+            
+//         }
+//         if(list1 == NULL)
+//             tail->next = list2;
+//         else
+//             tail->next = list1;
+//         return ans;
+//     }
+            
+        
+            
+
+        
+        
+};
