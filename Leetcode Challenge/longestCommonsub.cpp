fe43fbed1ec6c5119a908a213d56cbbc43082407
swastik2703 <swastikvasistha2127@gmail.com>
2022-10-20 18:40:32 +0530

    longest common subsequence

--- Leetcode Challenge/longestCommonsub.cpp
+++ Leetcode Challenge/longestCommonsub.cpp
@@ -20,9 +20,26 @@ public:
         return dp[i][j] = ans;
     }
     
+    int solveBU(string& a, string& b){
+        vector<vector<int>> dp(a.length()+1, vector<int>(b.length()+1,0));
+        for(int i=a.length()-1; i>=0; i--){
+            for(int j=b.length()-1; j>=0; j--){
+                int ans;
+                if(a[i] == b[j])
+                    ans = 1+dp[i+1][j+1];
+                else
+                    ans = max(dp[i+1][j],dp[i][j+1]);
+                dp[i][j] = ans;
+            }
+            
+        }
+        return dp[0][0];
+    }
+    
     int longestCommonSubsequence(string text1, string text2) {
         //function bnao jaha subsequence niklega
-        vector<vector<int>> dp(text1.length(), vector<int> (text2.length(), -1));
-        return solve(text1,text2,0,0,dp);
+        // vector<vector<int>> dp(text1.length(), vector<int> (text2.length(), -1));
+        // return solve(text1,text2,0,0,dp);
+        return solveBU(text1,text2);
     }
 };
